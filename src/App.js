ef906b19fb190f60b70d8944f2a901ba72d0e802
Loly Maluf <lolymaluf@gmail.com>
2022-05-18 19:09:37 -0300

    Adiciona rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route exact path ="/"></Route>
+        <Route path ="/album"></Route>
+        <Route path ="/favorites"></Route>
+        <Route path ="/profile"></Route>
+        <Route path = "/profile/edit"></Route>
+        <Route path = "/search"></Route>
+      </Switch>
+    );
   }
 }
 

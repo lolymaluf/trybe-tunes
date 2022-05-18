3b2d7968fcac03d9274a65ea7d5f3ee6750e5251
Loly Maluf <lolymaluf@gmail.com>
2022-05-18 19:49:32 -0300

    Adiciona o input e botão

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default class Login extends React.Component {
   render() {
-    return <div data-testid="page-login" />;
+    return (
+      <div data-testid="page-login">
+        <form data-testid="login-name-input">
+          <button data-testid="login-submit-button" type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }

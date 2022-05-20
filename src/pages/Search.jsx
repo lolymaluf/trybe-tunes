e5fc8b3dc473c1a892e2b74cb04c2c007340f25d
Loly Maluf <lolymaluf@gmail.com>
2022-05-20 18:18:39 -0300

    Adiciona requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,37 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default class Search extends React.Component {
+  state = {
+    artistName: '',
+  }
+
+  handleChange = (event) => {
+    /*       console.log(event.target.value); */
+    this.setState((prevState) => ({ ...prevState, artistName: event.target.value }));
+  }
+
   render() {
+    const { artistName } = this.state;
+    const minCharacter = 2;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
+        <form>
+          <input
+            data-testid="search-artist-input"
+            value={ artistName }
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ artistName.length < minCharacter }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }

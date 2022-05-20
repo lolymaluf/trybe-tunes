9eda53d808401b608b908364b69025b2b783a2a3
Loly Maluf <lolymaluf@gmail.com>
2022-05-19 23:57:45 -0300

    Mudanças no requisito 02

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
 import Loading from '../components/Loading';
@@ -11,89 +10,54 @@ export default class Login extends React.Component {
     isLoading: false,
     isLoggedOn: false,
   }
-  /*   this.state.name = inputName; */
-  /* utilizar sempre forma abaixo para alterar estado!
-    handleChange = (event) => {
-    console.log(event.target.value);
-    this.setState((prevState) => ({ ...prevState, name: event.target.value })); */
 
-  handleChange = ({ target }) => {
-  /*     console.log(event.target.value); */
-    const { name, value } = target;
-    this.setState(() => ({ [name]: value }));
+  /*   utilizar SEMPRE forma abaixo para alterar estado! */
+  handleChange = (event) => {
+    /*       console.log(event.target.value); */
+    this.setState((prevState) => ({ ...prevState, name: event.target.value }));
   }
 
-  /*   handleSubmit = (event) => {
+  handleSubmit = async () => {
     const { name } = this.state;
-    const { navigateSearch } = this.props.navigate;
-    console.log(`Um nome foi enviado:${name}`);
-    event.preventDefault();
-    navigateSearch('search');
-  } */
-
-  handleClick = async () => {
-    this.setState({
-      isLoading: true,
-    });
-    const { userName } = this.state;
-    await createUser({ name: userName });
-    const { history } = this.props;
-    history.push('/search');
-  };
-
-  OnClickButton = async () => {
-    const { name } = this.state;
-    this.setState({
-      isLoading: true,
-    });
+    this.setState((prevState) => ({ ...prevState, isLoading: true }));
     await createUser({ name });
-    this.setState({
-      isLoggedOn: true,
-    });
+    this.setState((prevState) => ({ ...prevState, isLoggedOn: true }));
   }
 
   render() {
     const { name, isLoading, isLoggedOn } = this.state;
     const minNumber = 3;
+    if (isLoggedOn) return <Redirect to="/search" />;
+    if (isLoading) return <Loading />;
     return (
-      <div>
+    /*       <div>
         { isLoggedOn && <Redirect to="/search" />}
         { isLoading ? (
           <Loading />
-        ) : (
-          <div data-testid="page-login">
-            <h1>Login</h1>
-            <form
-              onSubmit={ this.handleSubmit }
-            >
-              <input
-                name="name"
-                data-testid="login-name-input"
-                value={ name }
-                onChange={ this.handleChange }
-              />
-              <button
-                name="submitButton"
-                data-testid="login-submit-button"
-                type="submit"
-                disabled={ name.length < minNumber }
-                onClick={ this.OnClickButton }
-              >
-                Entrar
-              </button>
-            </form>
-          </div>
-        )}
+        ) : ( */
+      <div data-testid="page-login">
+        <h1>Login</h1>
+        <form
+          onSubmit={ this.handleSubmit }
+        >
+          <input
+            data-testid="login-name-input"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="login-submit-button"
+            type="submit"
+            disabled={ name.length < minNumber }
+            /*                 onClick={ this.OnClickButton } */
+          >
+            Entrar
+          </button>
+        </form>
+        {/*           </div>
+        )} */}
 
       </div>
     );
   }
 }
-
-Login.propTypes = {
-  history: PropTypes.func.isRequired,
-};
-
-/*     const { id } = this.props.match.params;
-    const number = 3;
-    if (+id > number) { return (<Redirect to="/search" />); } */
